3012d1df7f8d65dd5eca2ae4fe1627308af4f271
Turley <jturley@mc106036.ni.ad.newsint>
2015-12-30 16:41:47 +0000

    wip

--- src/hypertension/core.clj
+++ src/hypertension/core.clj
@@ -1,6 +1,58 @@
-(ns hypertension.core)
+(ns hypertension.core
+  (:require [clojure.string :as s]
+            [defun :refer [defun]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;;; default implementation of `ApiResource` protocol.
+;;; will construct URLs similar to the examples provided
+;;; in the json-api spec (https://jsonapi.org/format)
+
+;;; note that the URL formats here are NOT part of the spec:
+;;; that would defeat the point of hypermedia entirely.
+;;; They are merely as good a way to define URLs as any.(
+(defn- construct-full-url
+  [path base-url]
+  (let [normalized (s/replace base-url #"\/+$" "")]
+    (str normalized path)))
+
+(defun _resource-url
+  "Construct URL for resource from `:id` and `:resource-type` keys."
+  ([subject {:base-url base-url}]
+   (if-let [path (_resource-url subject)]
+     (construct-full-url path base-url)))
+
+  ([{:id id :resource-type type}] (str "/" type "/" id))
+
+  ([_] nil))
+
+(defn- qualified-resource
+  ([extra-fragment key [subject :as resource-url-args]]
+   (qualified-resource extra-fragment key resource-url-args name))
+  ([extra-fragment key [subject :as resource-url-args] f]
+   (if-let [base (apply _resource-url resource-url-args)]
+     (when (get-in subject [:related key])
+       (str base extra-fragment (f key))))))
+
+(defun _relationship-url
+  ([subject related-name & args]
+   (qualified-resource "/relationships/" related-name (cons subject args))))
+
+(defun _related-resource-url
+  ([subject [key id] & args]
+   (qualified-resource "/" key (cons subject args) #(str (name %) "/" id)))
+  ([subject key & args]
+   (qualified-resource "/" key (cons subject args)))
+  ([_ _] nil))
+
+(defprotocol ApiResource
+  (resource-url [self]))
+
+(defprotocol ApiRelatedResource
+  (related-resource-url [self related-resource]))
+
+(defprotocol ApiRelationship
+  (relationship-url [self related-name]))
+
+(extend clojure.lang.PersistentArrayMap
+  ApiResource {:resource-url _resource-url}
+  ApiRelatedResource {:related-resource-url _related-resource-url}
+  ApiRelationship {:relationship-url _relationship-url})
--- test/hypertension/core_test.clj
+++ test/hypertension/core_test.clj
@@ -1,11 +1,57 @@
 (ns hypertension.core-test
-  (:require [midje.sweet :refer :all]
-            [hypertension.core :refer :all]))
+  (:require  [midje.sweet :refer :all]
+             [hypertension.core :refer :all]))
 
-(facts "about `entity-response`"
-  (fact "it handles a basic record"
-    a => 1
-    (against-background
-     (around :checks
-             (let [a 1]
-               ?form)))))
+(facts "default implementation of resource-url"
+  (fact "returns nil for record with missing fields"
+    (resource-url {}) => nil
+    (resource-url {:id 1}) => nil
+    (resource-url {:resource-type "foo"}) => nil)
+
+  (let [subject {:id 1 :resource-type "foo"}]
+    (fact "constructs url path for resource"
+      (resource-url subject) => "/foo/1")
+    (facts "options"
+      (fact "will append to base-url"
+        (resource-url {} {:base-url "foo.com"}) => nil
+        (resource-url subject {:base-url "foo.com"}) => "foo.com/foo/1"
+        (resource-url subject {:base-url "foo.com/"}) => "foo.com/foo/1"))))
+
+(facts "default relationship-url"
+  (fact "returns nil for record with missing fields"
+    (relationship-url {} :bar) => nil
+    (relationship-url {:id 1} :bar) => nil
+    (relationship-url {:resource-type "foo"} :bar) => nil
+    (relationship-url {:resource-type "foo" :id 1} :bar) => nil)
+
+  (let [subject {:id 1
+                 :resource-type "foo"
+                 :related {:bar {}
+                           "baz" {}}}]
+    (fact "creates url for named relationship"
+      (relationship-url subject :bar) => "/foo/1/relationships/bar"
+      (relationship-url subject "baz") => "/foo/1/relationships/baz")
+    (fact "passes on :base-url option"
+      (relationship-url subject :bar {:base-url "foo.com"})
+      => "foo.com/foo/1/relationships/bar")))
+
+(facts "default related-resource-url"
+  (fact "returns nil for record with missing fields"
+    (related-resource-url {} :bar) => nil
+    (related-resource-url {:id 1} :bar) => nil
+    (related-resource-url {:resource-type "foo"} :bar) => nil
+    (related-resource-url {:resource-type "foo" :id 1} :bar) => nil)
+
+  (let [subject {:id 1
+                 :resource-type "foo"
+                 :related {:bar {:id 2
+                                 :resource-type "baz"}
+                           :quux [{:id 3 :resource-type "baz"}]}}]
+    (fact "creates url for named relationship"
+      (related-resource-url subject :bar) => "/foo/1/bar")
+
+    (fact "to-many relationship"
+      (related-resource-url subject [:bar 3]) => "/foo/1/bar/3")
+    (fact "passes on :base-url option"
+      (related-resource-url subject :bar {:base-url "foo.com"})
+      => "foo.com/foo/1/bar")))
