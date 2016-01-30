499e1d945c3de94c77565568170de56b6013a71a
James Turley <jamesturley1905@googlemail.com>
2016-01-30 17:47:47 +0000

    nearly there

--- src/hypertension/core.clj
+++ src/hypertension/core.clj
@@ -15,25 +15,27 @@
     (str normalized path)))
 
 (defun _resource-url
-  "Construct URL for resource from `:id` and `:resource-type` keys."
+  "Construct URL for resource from `:id` and `:type` keys."
   ([subject {:base-url base-url}]
    (if-let [path (_resource-url subject)]
      (construct-full-url path base-url)))
 
-  ([{:id id :resource-type type}] (str "/" type "/" id))
+  ([{:id id :type type}] (str "/" type "/" id))
 
   ([_] nil))
 
 (defn- qualified-resource
   ([extra-fragment key [subject :as resource-url-args]]
+   (println extra-fragment key resource-url-args)
    (qualified-resource extra-fragment key resource-url-args name))
   ([extra-fragment key [subject :as resource-url-args] f]
    (if-let [base (apply _resource-url resource-url-args)]
-     (when (get-in subject [:related key])
+     (when (get subject key)
        (str base extra-fragment (f key))))))
 
 (defun _relationship-url
   ([subject related-name & args]
+   (println related-name)
    (qualified-resource "/relationships/" related-name (cons subject args))))
 
 (defun _related-resource-url
@@ -44,20 +46,65 @@
   ([_ _] nil))
 
 (defprotocol ApiResource
-  (resource-url [self] [self opts]))
+  (resource-url [self] [self opts] "Construct url for resource itself"))
 
 (defprotocol ApiRelatedResource
-  (related-resource-url [self related-resource]))
+  (related-resource-url
+    [self related-resource] [self related-resource opts]
+    "construct url for related resource"))
 
 (defprotocol ApiRelationship
-  (relationship-url [self related-name] [self related-name opts]))
+  (relationship-url
+    [self related-name] [self related-name opts]
+    "Construct URL for relationship"))
 
-(extend-type java.lang.Object
+(extend-type clojure.lang.PersistentArrayMap
   ApiResource
-  (resource-url [self] (_resource-url self))
-  (resource-url [self opts] (_resource-url self opts))
+  (resource-url
+    ([self] (_resource-url self))
+    ([self opts] (_resource-url self opts)))
   ApiRelationship
-  (relationship-url [self related-name]
-    (_relationship-url self related-name))
-  (relationship-url [self related-name opts]
-    (_relationship-url self related-name opts)))
+  (relationship-url
+    ([self related-name]
+     (_relationship-url self related-name))
+    ([self related-name opts]
+     (_relationship-url self related-name opts)))
+  ApiRelatedResource
+  (related-resource-url
+    ([self related-resource] (_related-resource-url self related-resource))
+    ([self related-resource opts] (_related-resource-url self related-resource opts))))
+
+(defn- do-thing [resource related-objects]
+  (reduce (fn [acc item]
+            (println (get related-objects item))
+            (assoc acc item
+                   {:links {:self (relationship-url resource item)
+                            :related (related-resource-url resource (get related-objects item))}
+                    :data (get related-objects item)}))
+          {}
+          (keys related-objects)))
+
+(defn- parse-relationships [resource {related-keys :related-keys}]
+  (if related-keys
+    (let [attrs (get-in resource [:data :attributes])
+          related-objects (do-thing (:data resource) (select-keys attrs related-keys))
+          base-resource (assoc-in resource [:data :attributes]
+                                  (apply (partial dissoc attrs) related-keys))]
+      (assoc-in base-resource [:data :relationships] related-objects))
+    resource))
+
+
+
+(defn json-api-data
+  ([resource]
+   (let [attrs (dissoc resource :id :type)]
+     {:links {:self (resource-url resource)}
+      :data {:id (:id resource)
+             :type (:type resource)
+             :attributes attrs}}))
+  ([resource opts]
+   (-> (json-api-data resource)
+        (parse-relationships opts))))
+
+
+(related-resource-url {:id 5 :type "foo" :bar {:id 5 :type "bar"}} :bar)
--- test/hypertension/core_test.clj
+++ test/hypertension/core_test.clj
@@ -2,56 +2,95 @@
   (:require  [midje.sweet :refer :all]
              [hypertension.core :refer :all]))
 
+(defrecord Dummy [id type])
+(defrecord Dummy2 [id]
+  ApiResource
+  (resource-url [self] (str "bar"))
+  (resource-url [self opts] (str "baz")))
+
 (facts "default implementation of resource-url"
-       (fact "returns nil for record with missing fields"
-             (resource-url {}) =>
-             (resource-url {:id 1}) => nil
-             (resource-url {:resource-type "foo"}) => nil)
+  (fact "returns nil for record with missing fields"
+    (resource-url {}) => nil
+    (resource-url {:id 1}) => nil
+    (resource-url {:type "foo"}) => nil)
 
-       (let [subject {:id 1 :resource-type "foo"}]
-         (fact "constructs url path for resource"
-               (resource-url subject) => "/foo/1")
-         (facts "options"
-                (fact "will append to base-url"
-                      (resource-url {} {:base-url "foo.com"}) => nil
-                      (resource-url subject {:base-url "foo.com"}) => "foo.com/foo/1"
-                      (resource-url subject {:base-url "foo.com/"}) => "foo.com/foo/1")))
+  (let [subject {:id 1 :type "foo"}]
+    (fact "constructs url path for resource"
+      (resource-url subject) => "/foo/1")
+    (facts "options"
+      (fact "will append to base-url"
+        (resource-url {} {:base-url "foo.com"}) => nil
+        (resource-url subject {:base-url "foo.com"}) => "foo.com/foo/1"
+        (resource-url subject {:base-url "foo.com/"}) => "foo.com/foo/1")))
 
-       (facts "default relationship-url"
-              (fact "returns nil for record with missing fields"
-                    (relationship-url {} :bar) => nil
-                    (relationship-url {:id 1} :bar) => nil
-                    (relationship-url {:resource-type "foo"} :bar) => nil
-                    (relationship-url {:resource-type "foo" :id 1} :bar) => nil)
+  (facts "default relationship-url"
+    (fact "returns nil for record with missing fields"
+      (relationship-url {} :bar) => nil
+      (relationship-url {:id 1} :bar) => nil
+      (relationship-url {:type "foo"} :bar) => nil
+      (relationship-url {:type "foo" :id 1} :bar) => nil)
 
-              (let [subject {:id 1
-                             :resource-type "foo"
-                             :related {:bar {}
-                                       "baz" {}}}]
-                (fact "creates url for named relationship"
-                      (relationship-url subject :bar) => "/foo/1/relationships/bar"
-                      (relationship-url subject "baz") => "/foo/1/relationships/baz")
-                (fact "passes on :base-url option"
-                      (relationship-url subject :bar {:base-url "foo.com"})
-                      => "foo.com/foo/1/relationships/bar"))))
+    (let [subject {:id 1
+                   :type "foo"
+                   :bar {}
+                   "baz" {}}]
+      (fact "creates url for named relationship"
+        (relationship-url subject :bar) => "/foo/1/relationships/bar"
+        (relationship-url subject "baz") => "/foo/1/relationships/baz")
+      (fact "passes on :base-url option"
+        (relationship-url subject :bar {:base-url "foo.com"})
+        => "foo.com/foo/1/relationships/bar"))))
 
 (facts "default related-resource-url"
   (fact "returns nil for record with missing fields"
     (related-resource-url {} :bar) => nil
     (related-resource-url {:id 1} :bar) => nil
-    (related-resource-url {:resource-type "foo"} :bar) => nil
-    (related-resource-url {:resource-type "foo" :id 1} :bar) => nil)
+    (related-resource-url {:type "foo"} :bar) => nil
+    (related-resource-url {:type "foo" :id 1} :bar) => nil)
 
   (let [subject {:id 1
-                 :resource-type "foo"
-                 :related {:bar {:id 2
-                                 :resource-type "baz"}
-                           :quux [{:id 3 :resource-type "baz"}]}}]
+                 :type "foo"
+                 :bar {:id 2
+                       :type "baz"}
+                 :quux [{:id 3 :type "baz"}]}]
     (fact "creates url for named relationship"
       (related-resource-url subject :bar) => "/foo/1/bar")
-
     (fact "to-many relationship"
       (related-resource-url subject [:bar 3]) => "/foo/1/bar/3")
     (fact "passes on :base-url option"
       (related-resource-url subject :bar {:base-url "foo.com"})
       => "foo.com/foo/1/bar")))
+
+(facts "protocol"
+  (let [rec (->Dummy 5 "foo")]
+    (fact "default impl works with records"
+      (resource-url rec) => "/foo/5"))
+  (let [rec (->Dummy2 5)]
+    (fact "can be overridden"
+      (resource-url rec) => "bar"
+      (resource-url rec {}) => "baz")))
+
+(facts "json-api-data - one resource"
+  (fact "creates a valid JSON API data structure"
+    (json-api-data {:id 5 :type "foo"}) =>
+    {:data {:id 5
+            :type "foo"
+            :attributes {}}
+     :links {:self "/foo/5"}}
+    (json-api-data {:id 5 :type "foo" :bar "baz"}) =>
+    {:data {:id 5
+            :type "foo"
+            :attributes {:bar "baz"}}
+     :links {:self "/foo/5"}})
+  (fact "parses relationships"
+    (json-api-data {:id 5 :type "foo"
+                    :blah {:id 5 :type "baz"}}
+                   {:related-keys [:blah]}) =>
+                   {:data {:id 5
+                           :type "foo"
+                           :attributes {}
+                           :relationships
+                           {:blah {:links {:self "/foo/1/relationships/blah"
+                                          :related "/foo/1/blah"}
+                                  :data {:type "baz" :id 5}}}}
+                    :links {:self "/foo/5"}}))
